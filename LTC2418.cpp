aa081f0fb0aa2678a863ba1eb04469a839ea9715
Neal Pisenti <npisenti@gmail.com>
2015-12-05 15:39:06 -0500

    Initial outline of library; untested
    * implements reading of channel
    
    Future: Add in continuous read mode (ie, cylce through several channels asynchronously)

--- LTC2418.cpp
+++ LTC2418.cpp
@@ -37,22 +37,65 @@ private:
 #endif*/
 
 #include "LTC2418.h"
+#include "SPI.h"
 
-
-LTC2418::LTC2418(int cs) : _cs(cs){
-    pinMode(_cs, OUTPUT);
+LTC2418::LTC2418(int cs, int clk_speed) :
+_cs(cs)
+ {
+	pinMode(_cs, OUTPUT);
     digitalWrite(_cs, HIGH);
-}
     
-int32_t LTC2418::read(){
-    SPI.beginTransaction(_spi_settings);
+    _spi_settings = SPISettings(clk_speed, MSBFIRST, SPI_MODE0);
+}
+
+void LTC2418::set_calibration(float lsb, int offset_code){
+	_lsb = lsb;
+	_offset_code = offset_code;
+}
+
+int32_t LTC2418::read(dac_ch_t ch){
+	
+	SPI.beginTransaction(_spi_settings);
     digitalWrite(_cs, LOW);
-    byte cmd = (0x80); // 0b1000....
-    byte data[4];
-    // read in data
-    for (int i = 0; i < 4; i++){
-        data[i] = SPI.transfer(cmd);
+    
+    // write channel
+    for (int i = 0; i < 4; i ++){
+    	SPI.transfer(_ch_codes[ch]);
     }
     
-    // parse out data
-    data[0] & 0x
+    digitalWrite(_cs, HIGH);
+    delayMicroseconds(1);
+    
+    // now, read back voltage
+    digitalWrite(_cs, LOW);
+    // wait for EOC to go low, signalling end of conversion
+    while (digitalRead(MISO)){
+    	continue;
+    }
+    for (int i = 0; i < 4; i++){
+    	_data.bytes[i] = SPI.transfer(_ch_codes[ch]);
+    }
+	
+	digitalWrite(_cs, HIGH);
+    SPI.endTransaction();
+    
+    // convert data to meaningful reading
+    // should eventually implement parity, bounds checking, etc. here??
+    
+    // using example code from Linduino
+    float adc_voltage;
+    int32_t adc_code;
+    // 1) Bit-shift ADC code to the right 6 bits
+  	adc_code = _data.val>>6;   
+  	// 2) Convert ADC code from offset binary to binary
+  	adc_code -= 8388608;                                              
+   	// 3) Calculate voltage from ADC code, lsb, offset.
+  	adc_voltage=((float)adc_code + _offset_code)*(_lsb); 
+  	
+  	// dump voltage into reading for that channel
+  	_readings[ch].adc_code = adc_code;
+  	_readings[ch].value = adc_voltage;
+  	
+}
+
+
